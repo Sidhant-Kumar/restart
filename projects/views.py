fc27b86961d7b8a6912c08d4c65d3e8174732e97
sidhantk11153 <sidhantk11155@gmail.com>
2021-07-26 23:38:44 +0530

    Initial commit

---
+++ projects/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def project(request):
+    return HttpResponse('this is home page')
+
+
+def pro(request, pk):
+    return HttpResponse('this is a redirect from home page to page no :' + str(pk))
